ae8ab74aad3da82f84ec3d1f2c9ac973a9132d9c
canyouseeyang <1993346375@qq.com>
2020-07-01 16:54:25 +0800

    歌手

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,31 +3,31 @@ namespace Home\Controller;
 use Think\Controller;
 header("content-type:text/html;charset=utf-8");
 class IndexController extends Controller {
-	//获得轮播图片。
+	//鑾峰緱杞挱鍥剧墖銆�
 	public function getLoopImg(){
 		$model = D("Loop");
 		return $model->select();
 	}
-	//获得歌单所有歌曲。第一首歌的封面，播放次数。静态。
+	//鑾峰緱姝屽崟鎵�鏈夋瓕鏇层�傜涓�棣栨瓕鐨勫皝闈紝鎾斁娆℃暟銆傞潤鎬併��
 	public function getSongList(){
-		//注意这里首字母大写其余的小写。
+		//娉ㄦ剰杩欓噷棣栧瓧姣嶅ぇ鍐欏叾浣欑殑灏忓啓銆�
 		$model = D("Songlist");
 		$songModel = D("Song");
-		//注意这里的where条件限制。
-		$songList = $model->where("name = '歌单一'")->select();
-		//获取表单的所有的歌曲。
+		//娉ㄦ剰杩欓噷鐨剋here鏉′欢闄愬埗銆�
+		$songList = $model->where("name = '姝屽崟涓�'")->select();
+		//鑾峰彇琛ㄥ崟鐨勬墍鏈夌殑姝屾洸銆�
 		$viewcourts = 0;
 		foreach ($songList as $key => $value) {
-			//i就是一个数组？？？
+			//i灏辨槸涓�涓暟缁勶紵锛燂紵
 			 $i =  $songModel->where("id = "+$value["song_id"])->select();
-			//获取总播放次数
+			//鑾峰彇鎬绘挱鏀炬鏁�
 			 $viewcourts += $value['viewcourts'];
 		}
-		//获取第一首歌曲的封面
+		//鑾峰彇绗竴棣栨瓕鏇茬殑灏侀潰
 		$profile = $i[0]["profile"];
 		return array($i,$viewcourts,$profile);
 	}
-	//歌单点击播放时获取歌单的数据,返回歌单里面的所有歌曲信息。
+	//姝屽崟鐐瑰嚮鎾斁鏃惰幏鍙栨瓕鍗曠殑鏁版嵁,杩斿洖姝屽崟閲岄潰鐨勬墍鏈夋瓕鏇蹭俊鎭��
 	public function ajaxgetPlayList(){
 		$get = $_GET['name'];
 		$model = D("Songlist");
@@ -39,28 +39,28 @@ class IndexController extends Controller {
 		$song = json_encode($i);
 		print_r($song);
 	}
-	//获得歌曲信息
+	//鑾峰緱姝屾洸淇℃伅
 	public function songInfo(){
 		$get["name"] = $_GET['name'];
 		$model = D("song");
 		$songinfo = $model->where($get)->find();
-		//获取歌曲的歌名
+		//鑾峰彇姝屾洸鐨勬瓕鍚�
 		$songname = $songinfo['name'];
-		//获取歌曲地址
+		//鑾峰彇姝屾洸鍦板潃
 		$songsrc = $songinfo['src'];
-		//获取歌曲封面
+		//鑾峰彇姝屾洸灏侀潰
 		$songprofile = $songinfo['profile'];
-		//获取歌词，并对歌词进行处理
-		//获取歌词文件名称
+		//鑾峰彇姝岃瘝锛屽苟瀵规瓕璇嶈繘琛屽鐞�
+		//鑾峰彇姝岃瘝鏂囦欢鍚嶇О
 		$songpath = $songinfo['lyric'];
-		//歌词文件处理
-		//文件的路径中不能有空格。
+		//姝岃瘝鏂囦欢澶勭悊
+		//鏂囦欢鐨勮矾寰勪腑涓嶈兘鏈夌┖鏍笺��
 		$path= "http://localhost".__ROOT__."/Public/upload/song/lyric/".$songpath;
-		//将lrc文件解析为数组
+		//灏唋rc鏂囦欢瑙ｆ瀽涓烘暟缁�
 		$songarray = file($path);
-		//获取歌词数组
+		//鑾峰彇姝岃瘝鏁扮粍
 		$songlyric = array();
-		//获取对应的事件单位毫秒.
+		//鑾峰彇瀵瑰簲鐨勪簨浠跺崟浣嶆绉�.
 		$songTime = array();
 		foreach ($songarray as $key => $value) {
 			// echo trim(mb_substr($value,10))."<br />";
@@ -69,58 +69,58 @@ class IndexController extends Controller {
 			 $m = (int)trim(substr($value,1,2))*60*1000;
 			 $s = (float)trim(substr($value,4,5))*1000;
 			 $total = $m + $s;
-			//存放每一句歌词的时间。
+			//瀛樻斁姣忎竴鍙ユ瓕璇嶇殑鏃堕棿銆�
 			 array_push($songTime,$total);
-			//存放处理的歌词。
+			//瀛樻斁澶勭悊鐨勬瓕璇嶃��
 			 array_push($songlyric,trim(mb_substr($value,10)));
 		}
 		return array($songsrc,$songprofile,$songTime,$songlyric,$songname);
 	}
-	//获取正在播放的歌曲的时间信息
+	//鑾峰彇姝ｅ湪鎾斁鐨勬瓕鏇茬殑鏃堕棿淇℃伅
 	public function ajaxsongTime(){
 		$post["name"] = $_GET['name'];
 		$model = D("song");
 		$songinfo = $model->where($post)->find();
-		//获取歌词文件名称
+		//鑾峰彇姝岃瘝鏂囦欢鍚嶇О
 		$songpath = $songinfo['lyric'];
-		//歌词文件处理
-		//文件的路径中不能有空格。
+		//姝岃瘝鏂囦欢澶勭悊
+		//鏂囦欢鐨勮矾寰勪腑涓嶈兘鏈夌┖鏍笺��
 		$path= "http://localhost".__ROOT__."/Public/upload/song/lyric/".$songpath;
-		//将lrc文件解析为数组
+		//灏唋rc鏂囦欢瑙ｆ瀽涓烘暟缁�
 		$songarray = file($path);
 		$songlyric = array();
-		//获取对应的时间单位毫秒.
+		//鑾峰彇瀵瑰簲鐨勬椂闂村崟浣嶆绉�.
 		$songTime = array();
 		foreach ($songarray as $key => $value) {;
 			 $m = (int)trim(substr($value,1,2))*60*1000;
 			 $s = (float)trim(substr($value,4,5))*1000;
 			 $total = $m + $s;
-			//存放每一句歌词的时间。
+			//瀛樻斁姣忎竴鍙ユ瓕璇嶇殑鏃堕棿銆�
 			 array_push($songTime,$total);
 		}
 		$songscroll = json_encode($songTime);
 		echo $songscroll;
 	}
-	//获取歌曲的信息
+	//鑾峰彇姝屾洸鐨勪俊鎭�
 	public function ajaxsongInfo(){
 		$get = $_GET['name'];
 		$model = D("song");
 		$songinfo = $model->where("name ="+$get)->find();
-		//获取歌曲地址
+		//鑾峰彇姝屾洸鍦板潃
 		$songsrc = $songinfo['src'];
-		//获取歌曲封面
+		//鑾峰彇姝屾洸灏侀潰
 		$songprofile = $songinfo['profile'];
-		//获取歌词，并对歌词进行处理
-		//获取歌词文件名称
+		//鑾峰彇姝岃瘝锛屽苟瀵规瓕璇嶈繘琛屽鐞�
+		//鑾峰彇姝岃瘝鏂囦欢鍚嶇О
 		$songpath = $songinfo['lyric'];
-		//歌词文件处理
-		//文件的路径中不能有空格。
+		//姝岃瘝鏂囦欢澶勭悊
+		//鏂囦欢鐨勮矾寰勪腑涓嶈兘鏈夌┖鏍笺��
 		$path= "http://localhost".__ROOT__."/Public/upload/song/lyric/".$songpath;
-		//将lrc文件解析为数组
+		//灏唋rc鏂囦欢瑙ｆ瀽涓烘暟缁�
 		$songarray = file($path);
-		//获取歌词数组
+		//鑾峰彇姝岃瘝鏁扮粍
 		$songlyric = array();
-		//获取对应的事件单位毫秒.
+		//鑾峰彇瀵瑰簲鐨勪簨浠跺崟浣嶆绉�.
 		$songTime = array();
 		foreach ($songarray as $key => $value) {
 			// echo trim(mb_substr($value,10))."<br />";
@@ -129,17 +129,17 @@ class IndexController extends Controller {
 			 $m = (int)trim(substr($value,1,2))*60*1000;
 			 $s = (float)trim(substr($value,4,5))*1000;
 			 $total = $m + $s;
-			//存放每一句歌词的时间。
+			//瀛樻斁姣忎竴鍙ユ瓕璇嶇殑鏃堕棿銆�
 			 array_push($songTime,$total);
-			//存放处理的歌词。
+			//瀛樻斁澶勭悊鐨勬瓕璇嶃��
 			 array_push($songlyric,trim(mb_substr($value,10)));
 		}
 		return array($songsrc,$songprofile,$songTime,$songlyric,$songname);
 	}
 	public function Index(){
-		// 获得轮播的图片
+		// 鑾峰緱杞挱鐨勫浘鐗�
 		  $loopImg = $this->getLoopImg();
-		// 获得表单的数据
+		// 鑾峰緱琛ㄥ崟鐨勬暟鎹�
 		  $songList = $this->getSongList();
 		  $songInfo = $this->songInfo();
 		  // $songInfo2 = $songInfo[2];
@@ -149,4 +149,10 @@ class IndexController extends Controller {
 		  $this->assign("songList",$songList);
 		  $this->display();
 		  }
+   public function paihang() {
+   $this->display();
+   }
+   public function singer() {
+   $this->display();
+   }
 }
