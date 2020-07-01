8d2c4adc9bcc65349a377e48efa40f74bb7927f6
canyouseeyang <1993346375@qq.com>
2020-07-01 12:48:41 +0800

    排行榜

---
+++ Application/Application/Admin/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+	return array(
+		'USER_CONFIG'=>array(
+			'admin_name' => 'root1',
+			'admin_pwd' => 'root1',
+		)
+	);
---
+++ Application/Application/Common/Conf/config.php
@@ -0,0 +1,20 @@
+<?php
+// 应用配置，所有模块调用之前都会首先加载公共配置文件
+return array(
+	//'配置项'=>'配置值'
+	//数据库配置
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => 'localhost',
+	'DB_NAME' => 'kw',
+	'DB_USER' => 'root',
+	'DB_PWD' => 'root',
+	'DB_PORT' => '3306',
+	'DB_PREFIX' => '',
+	'DB_CHARSET' => 'utf8',
+	//URL的模式
+	'URL_MODEL' => 1,
+	 'DEFAULT_MODULE' => 'Admin',
+	 'DEFAULT_CONTROLLER' => 'Index',
+	 'DEFAULT_ACTION' => 'index',
+	 'URL_ROUTER_ON' => true,
+);
